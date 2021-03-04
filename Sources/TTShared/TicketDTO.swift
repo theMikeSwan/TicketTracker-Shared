7ff54cc5b032afbd67ca2c5cb82b54ea7a4f8f43
theMikeSwan <michaelswan@mac.com>
2021-03-04 12:53:32 -0500

    Initial add of TicketDTO and TicketStatus

---
+++ Sources/TTShared/TicketDTO.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Michael Swan on 3/2/21.
+//
+
+import Foundation
+
+public final class TicketDTO: Codable {
+        
+    public let id: UUID?
+    public let number: String
+    public var summary: String
+    public var detail: String
+    public var size: String
+    public let dateCreated: Date
+    public var status: TicketStatus
+    
+    public init(id: UUID?, number: String, summary: String, detail: String, size: String, dateCreated: Date, status: TicketStatus) {
+        self.id = id
+        self.number = number
+        self.summary = summary
+        self.detail = detail
+        self.size = size
+        self.dateCreated = dateCreated
+        self.status = status
+    }
+}
---
+++ Sources/TTShared/enums/TicketStatus.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Michael Swan on 3/2/21.
+//
+
+import Foundation
+
+public enum TicketStatus: String, Codable {
+    case todo
+    case inProgress
+    case inTest
+    case done
+
+}
